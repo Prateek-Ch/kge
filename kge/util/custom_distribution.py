e47995244299f34cbea5587cab330fa6494be601
PrateekCh <prateekchaturvedi216@gmail.com>
2024-11-05 16:12:38 +0100

    added distribution to it as well

--- kge/util/custom_distribution.py
+++ kge/util/custom_distribution.py
@@ -9,9 +9,13 @@ class CustomDistribution:
         self.checkpoint_path = 'local/experiments/20241021-193745-wnrr-rescal/checkpoint_best.pt'
         self.checkpoint = load_checkpoint(self.checkpoint_path)
         self.model = KgeModel.create_from(self.checkpoint)
-        self.current_dist_df = pd.DataFrame(
+        self.current_relation_count_df = pd.DataFrame(
             columns=["Relation ID", "Relation Strings", "Train Triple Count", "Valid Triple Count"
                      , "Test Triple Count"])
+        
+        self.current_relation_distribution_df = pd.DataFrame(
+            columns=["Relation ID", "Relation Strings", "Train Triple Distribution", "Valid Triple Distribution"
+                     , "Test Triple Distribution"])
     
     def current_distribution(self):
         # Dictionary to store relation counts for each split
@@ -23,23 +27,37 @@ class CustomDistribution:
         
         for key in relation_counts.keys():
             key_split = self.model.dataset.split(key)
+            relation_counts[key]["total_count"] = len(key_split)
             for triple in key_split:
                 relation = triple[1].item()
                 relation_counts[key][relation] += 1
         
         all_relation_ids = relation_counts["train"].keys()
         for relation_id in all_relation_ids:
-            relation_name = self.model.dataset.relation_strings(relation_id)
-            self.current_dist_df = self.current_dist_df._append({
-                "Relation ID": relation_id,
-                "Relation Strings": relation_name if relation_name else "",
-                "Train Triple Count": relation_counts["train"][relation_id],
-                "Valid Triple Count": relation_counts["valid"][relation_id],
-                "Test Triple Count": relation_counts["test"][relation_id]
-            }, ignore_index = True)
+            if relation_id != 'total_count':
+                relation_name = self.model.dataset.relation_strings(relation_id)
+                self.current_relation_count_df = self.current_relation_count_df._append({
+                    "Relation ID": relation_id,
+                    "Relation Strings": relation_name if relation_name else "",
+                    "Train Triple Count": relation_counts["train"][relation_id],
+                    "Valid Triple Count": relation_counts["valid"][relation_id],
+                    "Test Triple Count": relation_counts["test"][relation_id]
+                }, ignore_index = True)
 
-        print("\n Current Distribution: ")
-        print(self.current_dist_df)
+                self.current_relation_distribution_df = self.current_relation_distribution_df._append({
+                    "Relation ID": relation_id,
+                    "Relation Strings": relation_name if relation_name else "",
+                    "Train Triple Distribution": relation_counts["train"][relation_id] / relation_counts["train"]["total_count"],
+                    "Valid Triple Distribution": relation_counts["valid"][relation_id] / relation_counts["valid"]["total_count"],
+                    "Test Triple Distribution": relation_counts["test"][relation_id] / relation_counts["test"]["total_count"]
+                }, ignore_index = True)
+
+        print("\n Current Triple Count: ")
+        print(self.current_relation_count_df)
+
+        print("\n Current Triple Distribution: ")
+        print(self.current_relation_distribution_df)
+
 
 
 
