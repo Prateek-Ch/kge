a5192f4b125d52e2bd0859317e3f4002b544328f
PrateekCh <prateekchaturvedi216@gmail.com>
2025-01-25 20:02:22 +0100

    first graph but better

--- kge/util/distribution-shift/visualize.py
+++ kge/util/distribution-shift/visualize.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from matplotlib.ticker import MaxNLocator
 
 shift_files = {
     "0.01": "0.01/subgroup-log.csv",
@@ -18,21 +19,24 @@ columns = [
 for shift, file_path in shift_files.items():
     model = "distmult"
     file_path = f'./results/{model}/{file_path}'
-    df = pd.read_csv(file_path, header=None, names=columns, engine="python")
+    df = pd.read_csv(file_path, header=0, engine="python", names=columns)
     df["Shift"] = shift
     df["Model"] = model
     data.append(df)
 
 # Combine data into one DataFrame
 data = pd.concat(data, ignore_index=True)
+pd.set_option('display.max_rows', None)
 
 data["Shift"] = data["Shift"].astype(float)
+print(data["MR"])
 
 plt.figure(figsize=(12, 6))
-sns.lineplot(data=data, x="Shift", y="MR", hue="Relation Type", marker="o")
+sns.lineplot(data=data, x="Shift", y="MR", hue="Relation Type", marker="o", ci=None)
 plt.title("MR Across Noise Levels for Different Relation Types")
 plt.xlabel("Noise Level (Shift)")
 plt.ylabel("Mean Rank (MR)")
+plt.gca().yaxis.set_major_locator(MaxNLocator(integer=True, prune='lower', nbins=6))
 plt.legend(title="Relation Type", bbox_to_anchor=(1.05, 1), loc='upper left')
 plt.tight_layout()
 plt.show()
